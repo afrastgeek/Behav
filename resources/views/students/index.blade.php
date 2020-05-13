55db7f27e32cbb316f8065b36f68b1218201b538
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2020-05-13 19:17:29 +0700

    feature(student): create student index view
    > docs: https://laravel.com/docs/7.x/views#creating-views
    > docs: https://getbootstrap.com/docs/4.4/layout/grid/
    > docs: https://getbootstrap.com/docs/4.4/components/card/
    > docs: https://getbootstrap.com/docs/4.4/utilities/borders/
    > docs: https://getbootstrap.com/docs/4.4/utilities/shadows/
    > docs: https://getbootstrap.com/docs/4.4/utilities/spacing/
    > docs: https://getbootstrap.com/docs/4.4/content/tables/

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mb-4">Students</h1>
+            <div class="card border-0 shadow-sm">
+                <table class="table table-hover mb-0">
+                    <thead>
+                        <tr>
+                            <th class="border-top-0" scope="col">No.</th>
+                            <th class="border-top-0" scope="col">Student ID Number</th>
+                            <th class="border-top-0" scope="col">Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
