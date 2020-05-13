1c7ab4fc4a97abe7b8957f448ba1a46a90dd7fc2
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2020-05-13 16:38:52 +0700

    feature(student): add link to create new student
    > docs: https://getbootstrap.com/docs/4.4/utilities/spacing/
    > docs: https://laravel.com/docs/7.x/routing#named-routes
    > docs: https://getbootstrap.com/docs/4.4/components/buttons/

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -5,6 +5,9 @@
     <div class="row">
         <div class="col">
             <h1 class="mb-4">Students</h1>
+            <div class="mb-4">
+                <a href="{{ route('students.create') }}" class="btn btn-primary" role="button">Create Student</a>
+            </div>
             <div class="card border-0 shadow-sm">
                 <table class="table table-hover mb-0">
                     <thead>
