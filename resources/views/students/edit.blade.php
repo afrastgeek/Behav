d6d678cb7b289d793f3d6178c5d183e9b7302156
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2020-05-13 19:51:11 +0700

    refactor: create and use shared error sub-view for form views
    > docs: https://laravel.com/docs/7.x/blade#including-subviews

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -5,13 +5,9 @@
     <div class="row">
         <div class="col">
             <h1 class="mb-4">Students</h1>
-            @if ($errors->any())
-                @foreach ($errors->all() as $error)
-                    <div class="alert alert-danger" role="alert">
-                        {{ $error }}
-                    </div>
-                @endforeach
-            @endif
+
+            @include('shared.errors')
+
             <div class="card border-0 shadow-sm">
                 <form action="{{ route('students.update', $student->id) }}" method="post">
                     @csrf
