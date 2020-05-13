ffd646b14d46a1742ce8a32c6d2bae678f2daa55
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2020-05-13 18:55:23 +0700

    feature(student): update edited student data

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -78,7 +78,9 @@ class StudentController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+        $student->fill($request->all())->save();
+
+        return redirect('students');
     }
 
     /**
