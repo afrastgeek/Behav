eecc7d3e890a2612c5f66b9fa27f05a25c9c2aca
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2020-05-15 12:51:08 +0700

    chore: define activity model table

--- app/Activity.php
+++ app/Activity.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Activity extends Pivot
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'behavior_student';
 }
