65ce0903f1216c2640ec3e6620bd62886fc19f0f
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2020-05-15 12:53:20 +0700

    chore: define inverse relationship from activity model

--- app/Activity.php
+++ app/Activity.php
@@ -12,4 +12,20 @@ class Activity extends Pivot
      * @var string
      */
     protected $table = 'behavior_student';
+
+    /**
+     * Get the behavior that commited in the activity.
+     */
+    public function behavior()
+    {
+        return $this->belongsTo('App\Behavior');
+    }
+
+    /**
+     * Get the student that commited the activity.
+     */
+    public function student()
+    {
+        return $this->belongsTo('App\Student');
+    }
 }
